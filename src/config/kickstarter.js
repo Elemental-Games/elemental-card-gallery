e59fcead89451b3fc9cbcf4a74dbaf64e5eb3847
Elemental-Games <mmdiorio3@gmail.com>
2026-02-18 09:28:58 -0500

    update kickstarter config

--- src/config/kickstarter.js
+++ src/config/kickstarter.js
@@ -1,6 +1,6 @@
 export const kickstarterConfig = {
   goal: 50000,
-  raised: 4669,
+  raised: 5150,
   backers: 0,
   url: 'https://www.kickstarter.com/projects/elemental-games/elekin',
   stretchGoals: [
